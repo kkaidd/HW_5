ec9c6b5954a4c9322894a0173bdd15c58d02aec7
KMsulimanov <kaborzid@gmail.com>
2024-12-07 20:18:51 +0300

    Add test for checking "Enterprises" link in GitHub Enterprise dropdown

---
+++ src/test/java/GitHubEnterprisePageLoadTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubEnterprisePageLoadTest {
+    @Test
+
+    public void foundEnterprisePage() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+
+
+        open("https://github.com");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $("a[href=\"https://github.com/enterprise\"]").click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+
+    }
+
+}
