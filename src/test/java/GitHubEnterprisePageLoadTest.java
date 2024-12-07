33e2f33dbc45003b6c198f422adcfa1c3b892029
KMsulimanov <kaborzid@gmail.com>
2024-12-07 21:46:01 +0300

    update method name to foundEnterprisePageTests and remove unused imports

--- src/test/java/GitHubEnterprisePageLoadTest.java
+++ src/test/java/GitHubEnterprisePageLoadTest.java
@@ -2,8 +2,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
